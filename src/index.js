2b7ff78cd7c9adb27a76ad0ebe3e0f8ea133137b
Alan Ayala <aaga941228@outlook.es>
2020-05-31 14:06:17 -0500

    router

--- src/index.js
+++ src/index.js
@@ -2,19 +2,24 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const morgan = require("morgan");
 const path = require("path");
-const { PORT } = require("./config");
+const { port } = require("./config");
 
-express()
-  .engine(
-    ".hbs",
-    exphbs({
-      defaultLayout: "main",
-      layoutsDir: path.join(__dirname, "views/layouts"),
-      partialsDir: path.join(__dirname, "views/partials"),
-      extname: ".hbs",
-    })
-  )
-  .set("view engine", ".hbs")
-  .use(morgan("dev"))
-  .get("*", (req, res) => res.send("ok"))
-  .listen(PORT, () => console.log(`server on port: ${PORT}`));
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  exphbs({
+    defaultLayout: "main",
+    partialsDir: path.join(app.get("views"), "partials"),
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
+app.use(morgan("dev"));
+app.use(require("./routes"));
+app.use("/statics", express.static(path.join(__dirname, "public")));
+app.listen(port, () => {
+  console.log(`server on port ${port}`);
+});
